fd612688bb27469ab6e1de6997577766b0c2af50
Magz <magdy_176@hotmail.com>
2024-02-27 08:23:08 +0000

    naughts and crosses basic functions WIP

--- index.js
+++ index.js
@@ -2,68 +2,61 @@ let currentPlayer = "X";
 let count = 0;
 const playersTurn = document.getElementById("player-turn");
 
-const winningConditions = [
-  [1, 2, 3],
-  [1, 4, 7],
-  [1, 5, 9],
-  [2, 5, 8],
-  [3, 6, 9],
-  [3, 5, 7],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+const winningConditionsMet = (sign, playersTurnElement) => {
+  const cell1 = document.getElementById("cell1").innerText;
+  const cell2 = document.getElementById("cell2").innerText;
+  const cell3 = document.getElementById("cell3").innerText;
+  const cell4 = document.getElementById("cell4").innerText;
+  const cell5 = document.getElementById("cell5").innerText;
+  const cell6 = document.getElementById("cell6").innerText;
+  const cell7 = document.getElementById("cell7").innerText;
+  const cell8 = document.getElementById("cell8").innerText;
+  const cell9 = document.getElementById("cell9").innerText;
+  if (
+    (cell1 === sign && cell2 === sign && cell3 === sign) ||
+    (cell1 === sign && cell4 === sign && cell7 === sign) ||
+    (cell1 === sign && cell5 === sign && cell9 === sign) ||
+    (cell2 === sign && cell5 === sign && cell8 === sign) ||
+    (cell3 === sign && cell6 === sign && cell9 === sign) ||
+    (cell3 === sign && cell5 === sign && cell7 === sign) ||
+    (cell4 === sign && cell5 === sign && cell6 === sign) ||
+    (cell7 === sign && cell8 === sign && cell9 === sign)
+  ) {
+    playersTurnElement.innerText = `Player ${sign} WINS!`;
+    setTimeout(resetCells,1000);
+  }
+};
 
 let playerX = [];
 let playerO = [];
 
-const checkWin = (player) => {
-  return winningConditions.some((condition) => {
-    return condition.every((cell) => player.includes(cell));
-  });
-};
-
 for (let i = 1; i <= 9; i++) {
   let cell = document.getElementById("cell" + i);
-  console.log(cell.innerText);
   cell.addEventListener("click", () => {
     if (count === 0 && cell.innerText === "") {
       cell.innerText = "X";
       count += 1;
-      playersTurn.innerText = "Player 2, its your turn!";
+      playersTurn.innerText = "Player 2, it's your turn!";
       playerX.push(i);
-      console.log(playerX);
-      if (checkWin(playerX)) {
-        playersTurn.innerText = "Player 1 WINS!";
-        playerX = [];
-      }
+      winningConditionsMet("X", playersTurn);
+      playerX = [];
     } else if (count === 1 && cell.innerText === "") {
       cell.innerText = "O";
       count -= 1;
-      playersTurn.innerText = "Player 1, its your turn!";
+      playersTurn.innerText = "Player 1, it's your turn!";
       playerO.push(i);
-      console.log(playerO);
-      if (checkWin(playerO)) {
-        playersTurn.innerText = "Player 2 WINS!";
-        playerO = [];
-      }
+      winningConditionsMet("O", playersTurn);
+      playerO = [];
     }
   });
 }
 
 const resetCells = () => {
-    document.getElementById("cell1").innerText = "";
-    document.getElementById("cell2").innerText = "";
-    document.getElementById("cell3").innerText = "";
-    document.getElementById("cell4").innerText = "";
-    document.getElementById("cell5").innerText = "";
-    document.getElementById("cell6").innerText = "";
-    document.getElementById("cell7").innerText = "";
-    document.getElementById("cell8").innerText = "";
-    document.getElementById("cell9").innerText = "";
-    document.getElementById("player-turn").innerText = "Player 1, your playing as crosses!";
-}
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById("cell" + i).innerText = "";
+  }
+  playersTurn.innerText = "Player 1, you're playing as crosses!";
+};
 
-const button = document.getElementById("reset")
-button.addEventListener("click", () => {
-    resetCells();
-})
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", resetCells);
